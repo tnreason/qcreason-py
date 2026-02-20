90075969a9c0df409e0f7689c406da16ad89265b
Alex Goessmann <alex.goessmann@web.de>
2026-02-20 23:55:16 +0100

    Cleaned

--- old/logic_encoding.py
+++
@@ -1,6 +0,0 @@
-
-
-def get_atoms(formulaDict):
-    return set.union(*[extract_atoms_from_formula(formulaDict[formulaKey]) for formulaKey in formulaDict])
-
-
--- test/pennyLane_specDict_read.py
+++
@@ -1,6 +0,0 @@
-from qcreason.preparation import m2bp_connectives as mc
-
-from old import pennylane_circuits as pc
-
-print(mc.get_connective_operations("not","a","(not_a)")[0])
-print(pc.get_operation_from_unitaryDict(mc.get_connective_operations("not","a","(not_a)")[0]))
